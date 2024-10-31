a4ca63b58951fc15751171ceb4d49f82d00d9db3
weillercarvalho <engweiller@hotmail.com>
2024-10-31 16:09:36 -0300

    feat: Testing CI/CD Limit for free repos.

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ const tokens = [
   process.env.GITHUB_TOKEN2,
   process.env.GITHUB_TOKEN3,
   process.env.GITHUB_TOKEN4,
-  process.env.GITHUB_TOKEN5
+  process.env.GITHUB_TOKEN5,
 ];
 
 let tokenIndex = 0;
-let tokenAttempts = 0; 
 
 function getNextToken() {
   tokenIndex = (tokenIndex + 1) % tokens.length;
@@ -23,57 +22,34 @@ function getNextToken() {
     console.log("Error: Missing authentication token.");
     process.exit(1);
   }
-  
+
   console.log(`Using token: ${tokenIndex + 1}`);
   return token;
 }
 
 function getOctokitInstance() {
   return new Octokit({
-    auth: getNextToken()  // Ajuste para definir o token em cada instância do Octokit
+    auth: getNextToken(),
   });
-}
-
-async function checkAndHandleRateLimit(octokit, resource) {
-  try {
-    const rateLimit = await octokit.rateLimit.get();
-    const remaining = rateLimit.data.resources[resource].remaining;
-    const resetTime = rateLimit.data.resources[resource].reset * 1000;
-
-    if (remaining === 0) {
-      const currentTime = Date.now();
-      const waitTime = resetTime - currentTime;
-      console.log(`Limit ${resource} reached. Awaiting ${Math.ceil(waitTime / 1000)} seconds to reset...`);
-      await new Promise(resolve => setTimeout(resolve, waitTime));
-      return true;
-    }
-    return false;
-  } catch (error) {
-    console.error(`Error checking rate limit for ${resource}:`, error);
-    return true;
-  }
 }
 
 const limiter = new Bottleneck({
   maxConcurrent: 1,
-  minTime: 61000
+  minTime: 61000,
 });
 
 async function fetchReposWithLimiter(username) {
   let octokit = getOctokitInstance();
-  const exceeded = await checkAndHandleRateLimit(octokit, 'search');
-  if (!exceeded) {
-    await limiter.schedule(() => searchTopReposForEnvFile(octokit, username));
-  }
+  await limiter.schedule(() => searchTopReposForEnvFile(octokit, username));
 }
 
 async function searchTopReposForEnvFile(octokit, username) {
   try {
     const repos = await octokit.search.repos({
       q: `user:${username}`,
-      sort: 'stars',
-      order: 'desc',
-      per_page: 7
+      sort: "stars",
+      order: "desc",
+      per_page: 7,
     });
 
     if (repos.data.items.length === 0) {
@@ -86,12 +62,12 @@ async function searchTopReposForEnvFile(octokit, username) {
       console.log(`Checking repository: ${repoFullName}`);
 
       const codeSearch = await octokit.search.code({
-        q: `filename:.env repo:${repoFullName}`,  // Atualizado para procurar pelo arquivo .env
+        q: `filename:.env repo:${repoFullName}`,
       });
 
       if (codeSearch.data.items.length > 0) {
         console.log(`.env file found in repository ${repoFullName}`);
-        codeSearch.data.items.forEach(item => {
+        codeSearch.data.items.forEach((item) => {
           const filePath = item.path;
           console.log(`File found: ${filePath}`);
           saveRepoAndFileToFile(repoFullName, filePath);
@@ -106,7 +82,7 @@ async function searchTopReposForEnvFile(octokit, username) {
 }
 
 function saveRepoAndFileToFile(repoFullName, filePath) {
-  const outputFilePath = 'repos_found.txt';
+  const outputFilePath = "repos_found.txt";
   const logMessage = `Repository: ${repoFullName}, File: ${filePath}\n`;
   fs.appendFile(outputFilePath, logMessage, (err) => {
     if (err) {
@@ -118,17 +94,17 @@ function saveRepoAndFileToFile(repoFullName, filePath) {
 }
 
 function saveProcessedUser(user) {
-  const processedUsersFile = 'processed_users.txt';
-  fs.appendFileSync(processedUsersFile, `${user}\n`, 'utf-8');
+  const processedUsersFile = "processed_users.txt";
+  fs.appendFileSync(processedUsersFile, `${user}\n`, "utf-8");
 }
 
 function isUserProcessed(user) {
-  const processedUsersFile = 'processed_users.txt';
+  const processedUsersFile = "processed_users.txt";
   if (!fs.existsSync(processedUsersFile)) {
     return false;
   }
 
-  const processedUsers = fs.readFileSync(processedUsersFile, 'utf-8').split('\n');
+  const processedUsers = fs.readFileSync(processedUsersFile, "utf-8").split("\n");
   return processedUsers.includes(user);
 }
 
@@ -136,7 +112,7 @@ async function getTotalPages(octokit, range) {
   try {
     const response = await octokit.search.users({
       q: `followers:${range}`,
-      per_page: 1
+      per_page: 1,
     });
 
     const totalUsers = Math.min(response.data.total_count, 1000);
@@ -149,22 +125,20 @@ async function getTotalPages(octokit, range) {
 
 async function getRandomUserWithLimiter() {
   let octokit = getOctokitInstance();
-  const followerRanges = [
-    '1000..3000',
-    '3001..5000',
-    '5001..10000'
-  ];
+  const followerRanges = ["1000..3000", "3001..5000", "5001..10000"];
+  const maxUsersPerExecution = 2;
+  let usersProcessed = 0;
 
   for (const range of followerRanges) {
     const totalPages = await getTotalPages(octokit, range);
     if (totalPages === 0) continue;
 
-    for (let attempt = 0; attempt < 10; attempt++) { 
+    for (let attempt = 0; attempt < 7; attempt++) {
       const randomPage = Math.floor(Math.random() * totalPages) + 1;
       const users = await octokit.search.users({
         q: `followers:${range}`,
         per_page: 1,
-        page: randomPage
+        page: randomPage,
       });
 
       if (users.data.items.length === 0) continue;
@@ -175,7 +149,11 @@ async function getRandomUserWithLimiter() {
         console.log(`Checking user: ${user} in range ${range}`);
         await fetchReposWithLimiter(user);
         saveProcessedUser(user);
-        return;
+        usersProcessed++;
+
+        if (usersProcessed >= maxUsersPerExecution) return;
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
       } else {
         console.log(`User ${user} has already been processed previously. Skipping...`);
       }
@@ -184,11 +162,9 @@ async function getRandomUserWithLimiter() {
 }
 
 async function main() {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 3; i++) {
     await getRandomUserWithLimiter();
   }
 }
-
-main().catch(console.error);
 
 main().catch(console.error);
